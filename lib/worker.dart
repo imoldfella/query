5913452d675aa50a281246b74fa3b14e280cf481
Jim Hurd <jimh@datagrove.com>
2022-09-21 18:47:51 -0400

    loaf

--- lib/worker.dart
+++ lib/worker.dart
@@ -1,1 +1,46 @@
+@JS()
+library sample;
 
+// dart compile js -o web/worker.dart.js lib/worker.dart
+
+import 'dart:html';
+import 'package:js/js.dart';
+import './src/platform/worker.dart';
+
+
+@JS('self')
+external DedicatedWorkerGlobalScope get self;
+
+Object? _hello(Object j) { return 'world'; }
+
+const  basicSetup = <String, Object? Function(Object j)>{
+  'hello': _hello
+};
+
+class WorkerApp {
+  final sum = <int, Object?>{};
+  Map<String, Function(Object j)> method;
+  WorkerApp(this.method);
+}
+// main thread can call terminate(), or this thread can call close()
+void main() {
+
+  final w = WorkerApp(basicSetup);
+
+
+  // Wait for messages from the main isolate.
+  await for (final message in commandPort) {
+    if (message is ReductionJob) {
+      sum[message.tag] = message.params;
+    }
+  }
+}
+
+  self.onMessage.listen((e) {
+    
+    print('Message received from main script');
+    var workerResult = 'Result: ${e.data}';
+    print('Posting message back to main script');
+    self.postMessage(workerResult, null);
+  });
+}
--- worker/bin/worker.dart
+++
@@ -1,5 +0,0 @@
-import 'package:worker/worker.dart' as worker;
-
-void main(List<String> arguments) {
-  print('Hello world: ${worker.calculate()}!');
-}
